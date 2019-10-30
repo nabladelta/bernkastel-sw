c64f9cb4c7d1eccaeee2a40230e7b5697849ba86
reasv <7143787+reasv@users.noreply.github.com>
2019-10-30 12:06:38 +0100

    Add SW code

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,12 @@
-import ipfs from 'ipfs'
-export const ipfsOptions = {
-    repo: `./orbitdb/default/ipfs`,
-    relay: { enabled: true, hop: { enabled: true, active: true } },
-    EXPERIMENTAL: {
-      pubsub: true,
-      dht: true,
-    },
-    libp2p: {
-        config: {
-            dht: {
-                enabled: true
-            }
-        }
+const _self: SharedWorker.SharedWorkerGlobalScope = self as any
+import {initIPFS} from 'bernkastel'
+_self.onconnect = async e => {
+    let port = e.ports[0]
+    port.start()
+    var node = await initIPFS({})
+    port.onmessage = async e =>{
+        console.log(e.data)
+        let item = await node.get(e.data)
+        port.postMessage(item)
     }
-}
-async function init(){
-    let node = new ipfs(ipfsOptions)
-    node.on('error', error => console.error(error.message))
-    await new Promise((resolve, reject) => {node.on('ready', resolve)})
-    return node
-}
-var node: ipfs
-init().then(async (n: ipfs)=>{
-    node = n
-    console.log("started ipfs")
-    console.log(await node.cat("QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o"))
-})
-self.addEventListener('install', function(evt) {
-    console.log('The service worker is being installed.');  
-});
-  
+};

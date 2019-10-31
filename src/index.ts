08229aa1eff7db0eb2fe8fa6e474f19d782ee1e7
reasv <7143787+reasv@users.noreply.github.com>
2019-10-31 15:37:58 +0100

    First thread logic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
 const _self: SharedWorker.SharedWorkerGlobalScope = self as any
-import {initIPFS} from 'bernkastel'
+import {initIPFS, initOrbit, Thread} from 'bernkastel'
+import IPFS from 'ipfs'
+import OrbitDB from 'orbit-db'
+var threads: {[key: string]: Thread} = {}
+var ipfs: IPFS
+var orbit: OrbitDB
 _self.onconnect = async e => {
     let port = e.ports[0]
     port.start()
-    var node = await initIPFS({})
-    port.onmessage = async e =>{
+    if (!ipfs) ipfs = await initIPFS({})
+    if (!orbit) orbit = await initOrbit(ipfs, {})
+
+    port.onmessage = async e => {
         console.log(e.data)
-        let item = await node.get(e.data)
-        port.postMessage(item)
+        if (e.data.op === 'open'){
+            if (!threads[e.data.address]){
+                var options = {ipfs, orbit, address: undefined}
+                if (e.data.new) options.address = e.data.address
+                threads[e.data.address] = new Thread(options)
+                await threads[e.data.address].ready
+            }
+            threads[e.data.address].bindOnReplicated((a)=>port.postMessage(threads[e.data.address].posts))
+            port.postMessage(threads[e.data.address].posts)
+        }
+        if (e.data.op === 'post'){
+            if (!threads[e.data.address]){
+                port.postMessage({r: false, hash: null})
+                return
+            }
+            await threads[e.data.address].post({time: Date.now(), message: e.data.message})
+            console.log(threads[e.data.address].db.all)
+            port.postMessage(threads[e.data.address].posts)
+            return
+        }
+        if (e.data.op === 'connect'){
+            await ipfs.swarm.connect(e.data.peer)
+            return
+        }
     }
 };
